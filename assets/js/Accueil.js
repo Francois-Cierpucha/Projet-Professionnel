d6acb35abb803892891b1f02d7a6a5806576282e
Francois-Cierpucha <francoiscierpucha0gmail.com>
2019-11-20 16:23:20 +0100

    versions de mercredi

---
+++ assets/js/Accueil.js
@@ -0,0 +1,12 @@
+// Navbar
+window.onscroll = function() {navbarFunction()}
+let navbar = document.getElementById('navbar')
+let sticky = navbar.offsetTop;
+function navbarFunction(){
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+  }else{
+    navbar.classList.remove('sticky');
+  };
+};
+  
